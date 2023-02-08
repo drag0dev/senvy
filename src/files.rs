1336ff2496843c90439a9b05626db9694e728251
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-02-08 17:33:51 +0100

    implement reading, derive PartialEq for tests

--- src/files.rs
+++ src/files.rs
@@ -1,11 +1,14 @@
-use std::{fs::OpenOptions, io::Write};
+use std::{fs::OpenOptions, io::{Write, Read}};
 use anyhow::Result;
-use serde_json::to_vec;
+use serde_json::{
+    to_vec, from_str
+};
 use crate::types::{Project, ProjectEntry};
 
 // TODO: use atomicwrite for writing
+// TODO: use tokio async read
 
-/// prefix file names with "./data/"
+/// prefix file names with "data/"
 macro_rules! path_prefix {
     ( $x:expr ) => {
         {
@@ -47,8 +50,26 @@ pub async fn create(timestamp: u128, project_info: Project) -> Result<bool> {
 }
 
 /// reading already existing project
-pub async fn read(project_name: &str) -> Result<()> {
-    todo!();
+/// err indicates fs error, json error should not happen
+/// None means that file doesn't exist
+pub async fn read(project_name: &str) -> Result<Option<ProjectEntry>> {
+    let path = path_prefix!(project_name);
+    let file = OpenOptions::new()
+        .read(true)
+        .open(path);
+    if file.is_err() {
+        let err = file.err().unwrap();
+        match err.kind() {
+            std::io::ErrorKind::NotFound => return Ok(None),
+            _ => return Err(err.into()),
+        }
+    }
+
+    let mut file = file.unwrap();
+    let mut buff = String::new();
+    file.read_to_string(&mut buff)?;
+    let data: ProjectEntry = from_str(&buff)?;
+    Ok(Some(data))
 }
 
 /// updating already existing project
@@ -80,5 +101,25 @@ mod tests {
         // already existing file
         let res = create(123, data).await;
         assert_eq!(res.unwrap(), false);
+    }
+
+    #[actix_rt::test]
+    async fn read_file() {
+        let data = Project{
+            name: "test-read".to_string(),
+            vars: vec![
+                Var{name: "port".to_string(), value: "8080".to_string()}
+            ]};
+        _ = create(123, data.clone()).await.unwrap();
+
+        let data = ProjectEntry{
+            timestamp: 123,
+            vars: data.vars,
+        };
+        let res = read("test-read").await;
+        assert_eq!(Some(data), res.unwrap());
+
+        let res = read("test-read-not-existing").await;
+        assert_eq!(None, res.unwrap());
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
 use serde_derive::{Serialize, Deserialize};
 
 /// data about a project when creating a new one or updating already existing
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Project{
     /// project name
     pub name: String,
@@ -10,7 +10,7 @@ pub struct Project{
 
 /// name of the project is based on the file name
 /// timestamp of request arrival to avoid packages that come later but are faster to process
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct ProjectEntry {
     /// timestamp of the last change
     pub timestamp: u128,
@@ -18,7 +18,7 @@ pub struct ProjectEntry {
 }
 
 /// name value pair - env var
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Var {
     pub name: String,
     pub value: String,
