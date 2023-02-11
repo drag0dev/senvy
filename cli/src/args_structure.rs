d433aaea5272d3b4282771b68568c1642da2c078
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-02-11 20:26:23 +0100

    refactor args

--- cli/src/args_structure.rs
+++ cli/src/args_structure.rs
@@ -2,7 +2,7 @@ use clap::{Subcommand, Parser};
 
 #[derive(Debug, Parser)]
 #[command(name = "senvy")]
-#[command(about = "cli for server thath keeps your env vars", long_about = None)]
+#[command(about = "cli for senvy, server thath keeps your env vars", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
     command: Commands
@@ -11,42 +11,60 @@ pub struct Cli {
 #[derive(Debug, Subcommand)]
 pub enum Commands {
     #[command(about = "initialize senvy in your project")]
-    Init {},
+    Init {
+        #[arg(value_name = "project name")]
+        name: String,
+
+        #[arg(value_name = "path to the file with env vars")]
+        file: String,
+
+        #[arg(value_name = "server url")]
+        remote_url: String
+    },
 
     #[command(about = "create a new project entry on the server")]
     New {
         #[arg(value_name = "project name")]
         name: String,
-        #[arg(value_name = "path to the file with env vars",
-            require_equals = true)]
-        file: String
+
+        #[arg(value_name = "path to the file with env vars")]
+        file: String,
+
+        #[arg(value_name = "server url")]
+        remote_url: String,
     },
 
-    #[command(about = "delete project entry on the server")]
+    #[command(about = "delete project entry on the server, blank means current project")]
     Delete {
         #[arg(value_name = "project name")]
-        name: String
+        name: Option<String>,
+
+        #[arg(value_name = "server url")]
+        remote_url: Option<String>,
     },
 
-    #[command(about = "pull env vars from the server")]
+    #[command(about = "pull env vars from the server, blank means current project")]
     Pull {
         #[arg(value_name = "project name")]
-        name: String
+        name: Option<String>,
+
+        #[arg(value_name = "server url")]
+        remote_url: Option<String>,
     },
 
-    #[command(about = "push env vars to the server")]
+    #[command(about = "push env vars to the server, blank name current project")]
     Push {
         #[arg(value_name = "project name")]
-        name: String,
-        #[arg(value_name = "path to the file with env vars",
-            require_equals = true)]
-        file: String
+        name: Option<String>,
+
+        #[arg(value_name = "path to the file with env vars")]
+        file: Option<String>,
+
+        #[arg(value_name = "server url")]
+        remote_url: Option<String>,
     },
 
     // TODO: implement endpoint
     #[command(about = "check if there are new env vars available")]
-    Check {
-        #[arg(value_name = "project name")]
-        name: String
-    }
+    Check {}
 }
